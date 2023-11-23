5ea241ff3ddd2d43cc01a441f62354d23be5858f
junior1995-werj <62955161+junior1995-werj@users.noreply.github.com>
2023-11-23 20:20:42 -0300

    Add files via upload

---
+++ adm_bot_teams/static/assets/js/dashboard.js
@@ -0,0 +1,235 @@
+$(function () {
+
+  var dashboardElement = document.getElementById('dashboard_1');
+  var values = dashboardElement.getAttribute('value-day-chart');
+  var dashboardElement = document.getElementById('dashboard_2');
+  var days = dashboardElement.getAttribute('day-chart');
+
+  var values_days = JSON.parse(values)
+
+  var listaEmStringSemColchetes = days.slice(1, -1); // Remove os colchetes "[" e "]"
+  var list_days = listaEmStringSemColchetes.split(","); // Divide a string em elementos
+
+
+  var maiorValor = Math.max(...values_days);
+
+  var chart = {
+    series: [
+      { name: "Total", data: values_days }
+    ],
+
+    chart: {
+      type: "bar",
+      height: 345,
+      offsetX: -15,
+      toolbar: { show: true },
+      foreColor: "#adb0bb",
+      fontFamily: 'inherit',
+      sparkline: { enabled: false },
+    },
+
+
+    colors: ["#5D87FF", "#49BEFF"],
+
+
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "35%",
+        borderRadius: [6],
+        borderRadiusApplication: 'end',
+        borderRadiusWhenStacked: 'all'
+      },
+    },
+    markers: { size: 0 },
+
+    dataLabels: {
+      enabled: false,
+    },
+
+
+    legend: {
+      show: false,
+    },
+
+
+    grid: {
+      borderColor: "rgba(0,0,0,0.1)",
+      strokeDashArray: 3,
+      xaxis: {
+        lines: {
+          show: false,
+        },
+      },
+    },
+
+    xaxis: {
+      type: "category",
+      categories: list_days,
+      labels: {
+        style: { cssClass: "grey--text lighten-2--text fill-color" },
+      },
+    },
+
+
+    yaxis: {
+      show: true,
+      min: 0,
+      max: maiorValor+2,
+      tickAmount: 4,
+      labels: {
+        style: {
+          cssClass: "grey--text lighten-2--text fill-color",
+        },
+      },
+    },
+    stroke: {
+      show: true,
+      width: 3,
+      lineCap: "butt",
+      colors: ["transparent"],
+    },
+
+
+    tooltip: { theme: "light" },
+
+    responsive: [
+      {
+        breakpoint: 600,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 3,
+            }
+          },
+        }
+      }
+    ]
+
+
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), chart);
+  chart.render();
+
+
+  // =====================================
+  // Breakup
+  
+  // =====================================
+
+  var dashboardElement = document.getElementById('dashboard_3');
+  var years = dashboardElement.getAttribute('list-years');
+  var dashboardElement = document.getElementById('dashboard_4');
+  var values_years = dashboardElement.getAttribute('list-years-values');
+  
+  var list_years = JSON.parse(years)
+  var list_values_years = JSON.parse(values_years)
+
+  var breakup = {
+
+    color: "#adb5bd",
+    series: list_values_years,
+    labels: list_years,
+    chart: {
+      width: 180,
+      type: "donut",
+      fontFamily: "Plus Jakarta Sans', sans-serif",
+      foreColor: "#adb0bb",
+    },
+    plotOptions: {
+      pie: {
+        startAngle: 0,
+        endAngle: 360,
+        donut: {
+          size: '75%',
+        },
+      },
+    },
+    stroke: {
+      show: false,
+    },
+
+    dataLabels: {
+      enabled: false,
+    },
+
+    legend: {
+      show: false,
+    },
+    colors: ["#5D87FF", "#ecf2ff", "#F9F9FD"],
+
+    responsive: [
+      {
+        breakpoint: 991,
+        options: {
+          chart: {
+            width: 150,
+          },
+        },
+      },
+    ],
+    tooltip: {
+      theme: "dark",
+      fillSeriesColor: false,
+    },
+  };
+
+  var chart = new ApexCharts(document.querySelector("#breakup"), breakup);
+  chart.render();
+
+
+  var dashboardElement = document.getElementById('dashboard_5');
+  var month = dashboardElement.getAttribute('list-month-values');
+
+
+
+  var list_month = JSON.parse(month)
+
+  var maiorValor = Math.max(...list_month);
+
+  var earning = {
+    chart: {
+      id: "sparkline3",
+      type: "area",
+      height: 60,
+      sparkline: {
+        enabled: true,
+      },
+      group: "sparklines",
+      fontFamily: "Plus Jakarta Sans', sans-serif",
+      foreColor: "#adb0bb",
+    },
+    series: [
+      {
+        name: "Earnings",
+        color: "#49BEFF",
+        data: list_month,
+      },
+    ],
+    stroke: {
+      curve: "smooth",
+      width: 2,
+    },
+    fill: {
+      colors: ["#f3feff"],
+      type: "solid",
+      opacity: 0.05,
+    },
+
+    markers: {
+      size: 0,
+    },
+    tooltip: {
+      theme: "dark",
+      fixed: {
+        enabled: true,
+        position: "right",
+      },
+      x: {
+        show: false,
+      },
+    },
+  };
+  new ApexCharts(document.querySelector("#earning"), earning).render();
+})
